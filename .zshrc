a409401f79bddbf817e798de859a49a0a04811e3
progpra5800 <propraprj@gmail.com>
2018-05-14 20:34:55 +0900

    update set_rbenv.sh and .zshrc

--- .zshrc
+++ .zshrc
@@ -54,7 +54,7 @@ function mkcd(){
 
 # added by Anaconda3 installer
 export PATH="/home/yorimoto/anaconda3/bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="/home/yorimoto/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 alias proterminator='terminator -l proterminator && exit'
--- set_rbenv.sh
+++ set_rbenv.sh
@@ -1,10 +1,8 @@
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-mkdir -p ~/.rbenv/plugins
-~/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git
-~
-rbenv install --list
+ git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+ mkdir -p ~/.rbenv/plugins
+ git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 rbenv install 2.5.1
+# rbenv install --list
 rbenv global 2.5.1
 gem install rails --version="5.1.6"
 rbenv rehash
